91afcfe7a2f884ed32658928c027b79c3286692d
Joker-PCL <hr@pcl-polipharm.com>
2023-06-03 15:28:47 +0700

    ิbackup_update

--- Backup_PDF/Backup_gg__pdf.py
+++ Backup_PDF/Backup_gg__pdf.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import subprocess
 import io
+import json
 
 import pickle
 from googleapiclient.discovery import build
@@ -10,94 +11,128 @@ from googleapiclient.http import MediaIoBaseDownload
 from google.auth.transport.requests import Request
 
 from time import sleep
+import ctypes
 
 # กำหนด path ของ credentials.json ที่คุณดาวน์โหลดมา
-CREDENTIALS_DIR = 'D:\\Project VS Code\\Backup_GGSH_PDF\\credentials.json'
 SCOPES = ['https://www.googleapis.com/auth/drive']
-TOKEN_DIR = 'D:\\Project VS Code\\Backup_GGSH_PDF\\token.pickle'
+CREDENTIALS_DIR = 'credentials.json'
+TOKEN_DIR = 'token.pickle'
 
-
-# กำหนดค่าไฟล์ที่จะดาวน์โหลด
-# ระบุ ID ของโฟล์เดอร์ที่ต้องการดาวน์โหลด
-folder_id = '10JT2s9zd8pcmSj-kB2T5s-kFHlW58IE3'
 # กำหนด path ของโฟล์เดอร์ที่ต้องการบันทึกไฟล์ PDF
-output_folder = 'D:\\Project VS Code\\Backup_GGSH_PDF\\path\\ROOM'
+setting_json = "setting.json"
+pdf_folder = 'PDF'
 
+user32 = ctypes.windll.user32
+user32.SetProcessDPIAware()
+user32.SetWindowPos(user32.GetForegroundWindow(), None, 0, 0, 800, 200, 0x0002)
 
 def main():
-    print("กำลังเชื่อมต่อ.....")
-    try:
-        creds = None
-        if os.path.exists(TOKEN_DIR):
-            with open(TOKEN_DIR, 'rb') as token:
-                creds = pickle.load(token)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    CREDENTIALS_DIR, SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open(TOKEN_DIR, 'wb') as token:
-                pickle.dump(creds, token)
+    while True:
+        print("Connecting...")
+        try:
+            creds = None
+            if os.path.exists(TOKEN_DIR):
+                with open(TOKEN_DIR, 'rb') as token:
+                    creds = pickle.load(token)
+            # If there are no (valid) credentials available, let the user log in.
+            if not creds or not creds.valid:
+                if creds and creds.expired and creds.refresh_token:
+                    creds.refresh(Request())
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        CREDENTIALS_DIR, SCOPES)
+                    creds = flow.run_local_server(port=0)
+                # Save the credentials for the next run
+                with open(TOKEN_DIR, 'wb') as token:
+                    pickle.dump(creds, token)
 
-        # สร้าง Google Drive API service
-        service = build('drive', 'v3', credentials=creds)
+            # สร้าง Google Drive API service
+            service = build('drive', 'v3', credentials=creds)
 
-        # ดึงรายการไฟล์ในโฟล์เดอร์
-        query = f"'{folder_id}' in parents and mimeType='application/vnd.google-apps.spreadsheet'"
-        results = service.files().list(
-            q=query, fields="nextPageToken, files(id, name)").execute()
-        items = results.get('files', [])
+            with open(setting_json) as json_file:
+                # แปลง JSON ไฟล์ เป็น Python Dict
+                prog_dict = json.load(json_file)
 
-        # วนลูปเพื่อดาวน์โหลดไฟล์และบันทึกเป็นไฟล์ PDF
-        for item in items:
-            file_id = item['id']
-            file_name = item['name']
-            file_name = file_name.replace("/", "-")
+            for data in prog_dict:
+                folder_name = os.path.join(pdf_folder, data["folderName"])
+                folder_id = data["folder_id"]
 
-            # สร้างไฟล์ PDF และกำหนดค่า MediaIoBaseDownload
-            output_path = os.path.join(output_folder, f"{file_name}.pdf")
-            file = io.BytesIO()
-            downloader = MediaIoBaseDownload(file, service.files().export_media(
-                fileId=file_id, mimeType='application/pdf'))
-            done = False
+                downloadFile(service, folder_id, folder_name)
 
-            # ตรวจสอบว่าตำแหน่งและชื่อไฟล์ปลายทางนั้นมีอยู่จริง
-            if not os.path.exists(output_path):
-                # ดาวน์โหลดไฟล์และบันทึกเป็นไฟล์ PDF
-                while done is False:
-                    status, done = downloader.next_chunk()
-                    print(f"Download {file_name} {int(status.progress() * 100)}%")
+            # print("<<<<<< การสำรองข้อมูลเสร็จเรียบร้อยแล้ว >>>>>>>")
+            # print("โปรแกรมจะตรวจสอบและสำรองข้อมูลในอีก")
+            print("<<<<<< Data backup completed successfully >>>>>>>")
+            print("The program will check and backup the data again in")    
+            # print(f"You can view the files {pdf_folder}")
+            subprocess.run('explorer "{}"'.format(pdf_folder), shell=True)
+            
+            # ตั้งเวลาในการรันรอบโปรแกรม
+            breaktime(5, 30, 0)
 
-                # เขียนข้อมูลในไฟล์ PDF
-                file.seek(0)
+        except Exception as e:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("<<ERROR>>",e)
+            # print("เกิดข้อผิดพลาดระหว่างดาวน์โหลด กำลังเชื่อมต่อไหม่....")
+            print("An error occurred during the download. Reconnecting...")
+            breaktime(0, 0, 30) 
 
-                # สามารถใช้โค้ดต่อไปนี้เพื่อเปิดไฟล์ใหม่ในโหมด binary และทำการบันทึกไฟล์
-                with open(output_path, 'wb') as f:
-                    # ทำการบันทึกไฟล์ PDF ที่ต้องการดาวน์โหลด
-                    f.write(file.read())
-            else:
-                print("ไฟล์ที่กำหนดมีอยู่แล้ว")
+def breaktime(Hours=5, Min=0, Sec=0):
+    for i in range((Hours*3600)+(Min*60)+Sec, 0, -1):
+        hours = i // 3600  # หารเพื่อหาจำนวนชั่วโมง
+        minutes = (i % 3600) // 60  # หารเพื่อหาจำนวนนาที
+        seconds = i % 60  # หารเพื่อหาจำนวนวินาที
 
-            print(f"File saved to {output_path} \n")
-        
-        print("\n <<<<<< การสำรองข้อมูลเสร็จเรียบร้อยแล้ว >>>>>>>")
-        print("โปรแกรมจะปิดตัวลงใน...")
-        for i in range(5, 0, -1):
-            print(i, "วินาที")
-            sleep(1)
+        # print(f"{hours} ชั่วโมง {minutes} นาที {seconds} วินาที", end='\r')
+        time_str = f"Timer: {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"  # แปลงเป็น string และใช้ zfill() เพื่อเติมศูนย์ด้านหน้าตัวเลข
+        print(time_str, end='\r')
+        sleep(1)
 
-        # เรียกใช้คำสั่งเปิดโฟลเดอร์ของ Windows
-        subprocess.run('explorer "{}"'.format(output_folder), shell=True)
-        quit()
+def downloadFile(service, folder_id, folder_name):
+    # ดึงรายการไฟล์ในโฟล์เดอร์
+    query = f"'{folder_id}' in parents and mimeType='application/vnd.google-apps.spreadsheet'"
+    results = service.files().list(
+        q=query,
+        fields="nextPageToken, files(id, name)"
+    ).execute()
 
-    except Exception as e:
-        print(e)
-        print("เกิดข้อผิดพลาดระหว่างดาวน์โหลด กำลังเชื่อมต่อไหม่....")
-        main()
+    items = results.get('files', [])
+
+    # วนลูปเพื่อดาวน์โหลดไฟล์และบันทึกเป็นไฟล์ PDF
+    for item in items:
+        file_id = item['id']
+        file_name = item['name']
+        file_name = file_name.replace("/", "-")
+
+        output_path = os.path.join(folder_name, f"{file_name}.pdf")
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(
+            file,
+            service.files()
+            .export_media(
+                fileId=file_id,
+                mimeType='application/pdf'
+            )
+        )
+
+        done = False
+
+        # ตรวจสอบว่าตำแหน่งและชื่อไฟล์ปลายทางนั้นมีอยู่จริง
+        if not os.path.exists(output_path):
+            # ดาวน์โหลดไฟล์และบันทึกเป็นไฟล์ PDF
+            while done is False:
+                status, done = downloader.next_chunk()
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(f"Download {file_name} {int(status.progress() * 100)}%")
+                print(f"File saved to {output_path}", end='\n')
+            # เขียนข้อมูลในไฟล์ PDF
+            file.seek(0)
+
+            # สามารถใช้โค้ดต่อไปนี้เพื่อเปิดไฟล์ใหม่ในโหมด binary และทำการบันทึกไฟล์
+            with open(output_path, 'wb') as f:
+                # ทำการบันทึกไฟล์ PDF ที่ต้องการดาวน์โหลด
+                f.write(file.read())
+        else:
+            pass
 
 if __name__ == '__main__':
     main()
